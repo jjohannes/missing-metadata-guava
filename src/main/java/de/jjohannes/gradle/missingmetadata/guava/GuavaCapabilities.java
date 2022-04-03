494bd4627c887f02993a5d0dc4e9165074375211
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-04-03 14:57:44 +0200

    Add a strategy to resolve capability conflicts between Guava versions
    Fixes #4

--- src/main/java/de/jjohannes/gradle/missingmetadata/guava/GuavaCapabilities.java
+++ src/main/java/de/jjohannes/gradle/missingmetadata/guava/GuavaCapabilities.java
@@ -2,6 +2,7 @@ package de.jjohannes.gradle.missingmetadata.guava;
 
 public interface GuavaCapabilities {
     String CAPABILITY_GUAVA_GROUP              = "com.google.guava";
+    String CAPABILITY_GUAVA_NAME               = "guava";
     String CAPABILITY_GOOGLE_COLLECTIONS_GROUP = "com.google.collections";
     String CAPABILITY_GOOGLE_COLLECTIONS_NAME  = "google-collections";
     String CAPABILITY_LISTENABLEFUTURE_GROUP   = "com.google.guava";
--- src/main/java/de/jjohannes/gradle/missingmetadata/guava/GuavaCapabilitiesConflictResolutionStrategy.java
+++ src/main/java/de/jjohannes/gradle/missingmetadata/guava/GuavaCapabilitiesConflictResolutionStrategy.java
@@ -6,7 +6,7 @@ import org.gradle.api.artifacts.ComponentVariantIdentifier;
 import org.gradle.api.artifacts.ResolutionStrategy;
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
 
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static de.jjohannes.gradle.missingmetadata.guava.GuavaCapabilities.*;
@@ -22,14 +22,38 @@ public class GuavaCapabilitiesConflictResolutionStrategy implements Action<Resol
             capabilitiesResolution.withCapability(
                     CAPABILITY_LISTENABLEFUTURE_GROUP, CAPABILITY_LISTENABLEFUTURE_NAME,
                     GuavaCapabilitiesConflictResolutionStrategy::selectGuava);
+            capabilitiesResolution.withCapability(
+                    CAPABILITY_GUAVA_GROUP, CAPABILITY_GUAVA_NAME,
+                    GuavaCapabilitiesConflictResolutionStrategy::selectGuava);
         });
     }
 
     private static void selectGuava(CapabilityResolutionDetails details) {
-        Set<ComponentVariantIdentifier> guava = details.getCandidates().stream().filter(id ->
-                ((ModuleComponentIdentifier) id.getId()).getModule().equals("guava")).collect(Collectors.toSet());
+        List<ComponentVariantIdentifier> guava = details.getCandidates().stream().filter(id ->
+                ((ModuleComponentIdentifier) id.getId()).getModule().equals("guava")).collect(Collectors.toList());
         if (guava.size() == 1) {
-            details.select(guava.iterator().next());
+            details.select(guava.get(0));
+        } else if (guava.size() > 1) {
+            // If there are more than one 'Guava' in the conflict, select the highest version.
+            // It is unclear how this happens, because usually the highest version of a component should have been selected before.
+            // See: https://github.com/jjohannes/missing-metadata-guava/issues/4
+            details.select(findHighestVersionCandidate(guava));
         }
+    }
+
+    private static ComponentVariantIdentifier findHighestVersionCandidate(List<ComponentVariantIdentifier> candidates) {
+        ComponentVariantIdentifier selected = candidates.get(0);
+        for (int i = 1; i < candidates.size(); i++) {
+            ComponentVariantIdentifier next = candidates.get(i);
+            if (next.getId() instanceof ModuleComponentIdentifier) {
+                String selectedVersion = ((ModuleComponentIdentifier) selected.getId()).getVersion();
+                String nextVersion = ((ModuleComponentIdentifier) next.getId()).getVersion();
+
+                if (nextVersion.compareTo(selectedVersion) > 0) {
+                    selected = next;
+                }
+            }
+        }
+        return selected;
     }
 }
